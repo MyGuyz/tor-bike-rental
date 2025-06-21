d97a7c2305ae95ae350ee15b54e100945d2e94c4
MyGuyz <natthakorn_1412@hotmail.com>
2025-06-21 16:20:27 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
